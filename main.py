8cbce721bfbdf9c39490af15ec48a1ae65530e19
Parker Shanklin <pshanklin1655@gmail.com>
2025-07-16 10:40:34 -0500

    Amaar is a cool guy!

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ print("This is a simple Python script.")
 print("My name is Parker and Amaar is my friend.")
 for i in range(5):
     print("Da LAW!")
+
+#amaar is a cool guy!
+print("This is the end of the script.")
+print("Goodbye!")
